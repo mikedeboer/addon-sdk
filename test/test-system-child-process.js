c037002ff63ec0d2f0e25fae0f164c0dd0d067ef
Jordan Santell <jsantell@gmail.com>
2013-08-15 15:35:16 -0700

    Add child event tests for spawning

--- test/test-system-child-process.js
+++ test/test-system-child-process.js
@@ -22,6 +22,8 @@ exports.testExecCallbackSuccess = function (assert, done) {
 
 exports.testExecCallbackError = function (assert, done) {
   exec('not-real-command', { cwd: SDK_ROOT }, function (err, stdout, stderr) {
+    assert.ok(/not-real-command/.test(err.toString()),
+      'error contains error message');
     assert.ok(err.code && isNumber(err.code), 'non-zero error code property on error');
     assert.equal(err.signal, null,
       'null signal property when not manually terminated');
@@ -56,12 +58,14 @@ exports.testExecFileCallbackSuccess = function (assert, done) {
 
 exports.testExecFileCallbackError = function (assert, done) {
   execFile('not-real-command', { cwd: SDK_ROOT }, function (err, stdout, stderr) {
+    assert.ok(/NS_ERROR_FILE_UNRECOGNIZED_PATH/.test(err.toString()),
+      'error contains error message');
     assert.equal(err.code, 'NS_ERROR_FILE_UNRECOGNIZED_PATH',
       'error code is NS_ERROR message');
     assert.equal(err.signal, null,
       'null signal property when not manually terminated');
     assert.equal(stdout, '', 'stdout is empty');
-    assert.ok(/NS_ERROR_FILE_UNRECOGNIZED_PATH/.test(stderr), 'stderr contains error message');
+    assert.equal(stderr, '', 'stdout is empty');
     done();
   });
 };
@@ -79,6 +83,52 @@ exports.testExecFileOptions = function (assert, done) {
     done();
   });
 };
+
+exports.testExecFileOptionsTimeout = function (assert, done) {
+  execFile(getScript('wait'), { timeout: 1000 }, (err, stdout, stderr) => {
+    assert.equal(stderr, '', 'stderr is empty');
+    assert.equal(err.killed, true, 'error has `killed` property as true');
+    assert.equal(err.code, null, 'error has `code` as null');
+    assert.equal(err.signal, 'SIGTERM',
+      'error has `signal` as SIGTERM by default');
+    assert.equal(stdout, '', 'stdout is empty');
+    assert.equal(stderr, '', 'stderr is empty');
+    done();
+  });
+};
+
+/*
+ * This tests failures when an error is thrown attempting to
+ * spawn the process, like an invalid command
+ */
+exports.testChildEventsSpawningError= function (assert, done) {
+  let handlersCalled = 0;
+  let child = execFile('i-do-not-exist', (err, stdout, stderr) => {
+    assert.ok(err);
+    assert.equal(stdout, '')
+    assert.equal(stderr, '');
+  });
+
+  child.on('error', handleError);
+  child.on('exit', handleExit);
+  child.on('close', handleClose);
+
+  function handleError (e) {
+    assert.ok(e, 'error passed into error handler');
+    if (++handlersCalled === 2) done();
+  }
+
+  function handleExit (code, signal) {
+    assert.equal(code, -1,
+      'process was never spawned, therefore exit code is -1');
+    assert.equal(signal, null, 'signal should be null');
+    if (++handlersCalled === 2) done();
+  }
+
+  function handleClose (code, signal) {
+    assert.fail('Close event should not be called on init failure');
+  }
+}
 
 function getScript (name) {
   let path = SDK_ROOT + '/test/fixtures/child-process/'
